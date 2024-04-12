3017cee6ad47dda4d93779ee72300bde299e1277
oohara <oohara@DESKTOP-GR7T929>
2024-04-12 15:18:13 +0900

    4月12日

--- src/bean/Student.java
+++ src/bean/Student.java
@@ -12,5 +12,38 @@ public class Student implements Serializable {
 	public String getNo() {
 		return no;
 	}
-	public void setNo()
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+    public int getEntYear() {
+    	return entYear;
+    }
+    public void setEntYear(int entYear) {
+    	this.entYear = entYear;
+    }
+    public String getClassNum() {
+    	return classNum;
+    }
+    public void setClassNum(String classNum) {
+    	this.classNum = classNum;
+    }
+    public boolean isAttend() {
+    	return isAttend;
+    }
+    public void setAttend(boolean isAttend) {
+    	this.isAttend = isAttend;
+    }
+    public School getSchool() {
+    	return school;
+    }
+    public void setSchool(School school) {
+    	this.school = school;
+    }
+
 }
