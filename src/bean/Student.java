fcfe5a3ac643f7cd09aab578139886a95808f787
oohara <oohara@DESKTOP-GR7T929>
2024-04-12 11:39:19 +0900

    examプロジェクトの新規登録

---
+++ src/bean/Student.java
@@ -0,0 +1,16 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+	private String no;
+	private String name;
+	private int entYear;
+	private String classNum;
+	private boolean isAttend;
+	private School school;
+	public String getNo() {
+		return no;
+	}
+	public void setNo()
+}
